e3256a90b1e6bf53dba29caf6a65f1972ef8d540
K3V3L <kovalsergii6@gmail.com>
2019-09-18 14:12:43 +0300

    update

--- source.cpp
+++ source.cpp
@@ -1,19 +1,19 @@
-//Jeff Chastine
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 //#include <GL/glext.h>
 #include <iostream>
+
 #define DELTA 0.05
-using namespace std;
+
 void changeSize(int w, int h);
 void render(void);
-
 void keyboard(unsigned char c, int x, int y);
-
 void mouse(int button, int state, int x, int y);
+
 double vpx=0,vpy=0;
+
 int main(int argc, char** argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB);
@@ -26,6 +26,7 @@ int main(int argc, char** argv) {
   glutReshapeFunc(changeSize);
   glutMainLoop();
 }
+
 void drawShape(){
   glBegin(GL_TRIANGLES);
   glVertex2f(-0.82,-0.08);
@@ -65,6 +66,7 @@ void drawShape(){
   glEnd();
 
 }
+
 void render(void) {
   glLoadIdentity();
   glOrtho(-1+vpx, 1+vpx, -1+vpy, 1+vpy, -1, 1);
@@ -72,7 +74,6 @@ void render(void) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glColor3f((float)147/255, (float)128/255, (float)4/255);
   glPolygonMode(GL_FRONT, GL_FILL);
-  //glOrtho(-1.1,1.1,-1.1,1.1,-1,1);
   drawShape();
   glutSwapBuffers();
 }
@@ -84,13 +85,13 @@ void keyboard(unsigned char c, int x, int y)
   case 43:
     vpx+=DELTA;
     vpy+=DELTA;
-    std::cout << "+" << endl;
+    std::cout << "+" << std::endl;
     break;
 
   case 45:
     vpx-=DELTA;
     vpy-=DELTA;
-    std::cout << "-" << endl;
+    std::cout << "-" << std::endl;
     break;
   
   case 133:
@@ -99,7 +100,7 @@ void keyboard(unsigned char c, int x, int y)
     break;
   }
 
-  cout << -1+vpx << " | " << 1+vpx << " | " << -1+vpy << " | " << 1+vpy << std::endl;
+  std::cout << -1+vpx << " | " << 1+vpx << " | " << -1+vpy << " | " << 1+vpy << std::endl;
   glutPostRedisplay();
 }
 
